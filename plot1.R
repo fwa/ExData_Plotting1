b05c5afcf83fa610f7d12121ce62eb96a4a4d8fd
fwa <fwa@users.noreply.github.com>
2014-05-12 08:17:36 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,1 +1,9 @@
- 
+data <- read.table('household_power_consumption.txt',header = T,sep=';',stringsAsFactor = F)
+sub <- data[data$Date %in% c('1/2/2007','21/2/2007'),]
+
+sub$Global_active_power <- as.numeric(sub$Global_active_power)
+
+hist(sub$Global_active_power,col='red', main = 'Global Active Power', xlab='Global Active Power(kilowatts)')
+
+dev.copy(png, file = "plot1.png") ## Copy my plot to a PNG file
+dev.off() ## close the PNG device
